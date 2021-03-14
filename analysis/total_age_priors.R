fe29f00498d1d6c95bbc59561acc67d38041b9d3
ebuhle <eric.buhle@noaa.gov>
2021-03-14 14:13:05 -0700

    Compute prior cell probabilities on establishment year for each tree
    Also (partially) improve recruitment time series plots.

--- analysis/total_age_priors.R
+++ analysis/total_age_priors.R
@@ -44,7 +44,16 @@ tree_age <- left_join(add_rings_height, rings_to_pith, by = c("patch","tree","it
          age = round(ring_count + add_rings_height + rings_to_pith),
          establishment_year = year - age - 1) # -1 b/c sampling is early in calendar year
 
-# Posterior distribution of number of trees established per year
+# Prior on establishment year of each tree
+# (for time-uncertain state-space model)
+tree_year <- pivot_wider(tree_age, id_cols = establishment_year, names_from = tree, 
+                         values_from = iter, values_fn = length, values_fill = 0) %>% 
+  complete(establishment_year = min(establishment_year):max(establishment_year)) %>% 
+  as.data.frame() %>% arrange(establishment_year) %>% replace(is.na(.), 0)
+
+tree_year[,-1] <- sweep(tree_year[,-1], 2, colSums(tree_year[,-1]), "/")
+
+# Prior on number of trees established per year
 recruitment <- as.data.frame(xtabs(~ iter + establishment_year + patch, tree_age)) %>% 
   rename(year = establishment_year, N = Freq) %>% 
   mutate(year = as.numeric(as.character(year)), iter = as.numeric(as.character(iter)))
@@ -69,8 +78,7 @@ tree_age %>% pivot_wider(id_cols = c(tree,iter), names_from = tree, values_from 
   geom_linerange(aes(ymin = l, ymax = h), size = 1.5, color = "darkgray") +
   geom_linerange(aes(ymin = ll, ymax = hh), size = 0.5, color = "darkgray") +
   geom_point(pch = 16, size = 1, alpha = 0.5) +
-  xlab("Tree") + ylab("Age") + 
-  theme_bw(base_size = 16) + 
+  labs(x = "Tree", y = "Age") + theme_bw(base_size = 16) + 
   theme(axis.ticks.x = element_blank(), axis.text.x = element_blank(),
         panel.grid = element_blank()) + 
   facet_wrap(vars(patch), scales = "free_x")
@@ -88,7 +96,7 @@ tree_age %>% mutate(tree = fct_reorder(.f = tree, .x = age, .fun = median)) %>%
   ggplot(aes(x = age, y = tree, height = stat(density))) + 
   geom_density_ridges(color = "white", fill = "black") +
   scale_x_log10(expand = expansion(0,0)) + scale_y_discrete(expand = expansion(mult = c(0.02,0.08))) +
-  xlab("Age") + ylab("Tree") + theme_bw(base_size = 16) + 
+  labs(x = "Age", y ="Tree") + theme_bw(base_size = 16) + 
   theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(), panel.grid = element_blank(), 
         panel.background = element_rect(fill = "black", color = "black"), panel.border = element_blank(),
         strip.background = element_rect(color = "black", fill = "black"),
@@ -99,11 +107,11 @@ if(save_plot) dev.off()
 
 # Credible intervals of posterior distribution of annual recruitment
 # Note: takes a while
-save_plot <- TRUE
+save_plot <- FALSE
 if(save_plot) {
   png(filename=here("analysis", "results", "recruitment_intervals.png"),
-      width=7, height=7, units="in", res=300, type="cairo-png")
-} else dev.new()
+      width=8, height=6, units="in", res=300, type="cairo-png")
+} else dev.new(width=8, height=6)
 
 recruitment %>% 
   pivot_wider(id_cols = c(patch,year,iter), names_from = c(patch,year), values_from = N, values_fill = 0) %>%
@@ -111,11 +119,13 @@ recruitment %>%
   mcmc_intervals_data(point_est = "mean", prob = 0.8, prob_outer = 0.95) %>%
   separate(parameter, c("patch","year"), sep = "_") %>% mutate(year = as.numeric(year)) %>% 
   ggplot(aes(x = year, y = m)) +
-  geom_ribbon(aes(ymin = ll, ymax = hh), fill = "darkgray", alpha = 0.5) +
-  geom_line(alpha = 0.5) +
-  xlab("Year") + ylab("Recruitment") + 
-  theme_bw(base_size = 16) + theme(panel.grid = element_blank()) +
-  facet_wrap(vars(patch))
+  geom_ribbon(aes(ymin = ll, ymax = hh), fill = "black", alpha = 0.3) +
+  geom_line(alpha = 0.5, lwd = 1) +
+  scale_y_continuous(breaks = seq(0, max(hh))) +
+  labs(x = "Year", y = "Recruitment") + theme_bw(base_size = 16) + 
+  theme(panel.grid = element_blank(), plot.margin = unit(c(5.5, 15, 5.5, 5.5), "points"),
+        panel.spacing = unit(15, "points")) +
+  facet_wrap(vars(patch), scales = "free_y")
 
 if(save_plot) dev.off()
 
